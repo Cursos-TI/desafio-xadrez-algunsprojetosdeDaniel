c325e8f3826bd6344f0dc53993a696b1ae266534
algunsprojetosdeDaniel <lojamarcketplaces@gmail.com>
2025-10-18 19:05:07 +0000

    loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
 int main () {
-    int torre, bispo, rainha;
+    int torre, bispo, rainha, cavalo1, cavalo2;
 
-    printf("o bispo vai andar 5 casas para a direita");
+    printf("A torre vai andar 5 casas para a direita\n");
 
     torre = 1;
 
     while (torre <= 5) {
-        printf("direita");
+        printf("Direita\n");
         torre++;
     }
 
-    printf("o bispo vai andar 5 casas na diagonal");
+    printf("O bispo vai andar 5 casas na diagonal\n");
 
     bispo = 1;
 
     do {
-        printf("cima");
-        printf("direita");
+        printf("Cima\n");
+        printf("Direita\n");
         bispo++;
     } while (bispo <= 5);
 
-    printf("a rainha vai andar 8 casas para a esquerda");
+    printf("A rainha vai andar 8 casas para a esquerda\n");
 
     rainha = 1;
 
     for (rainha; rainha <= 8; rainha++) {
-        printf("esquerda");
+        printf("esquerda\n");
+    }
+
+    printf("O cavalo vai andar 2 casas para baixo e 1 casa para a esquerda\n");
+
+    cavalo1 = 1;
+    cavalo2 = 1;
+
+    for (cavalo2; cavalo2 <= 1; cavalo2++) {
+        do {
+            printf("Baixo\n");
+            cavalo1++;
+            
+        } while (cavalo1 <= 2);
+        printf("Esquerda\n");
+        cavalo2++;
     }
 
     return 0;
