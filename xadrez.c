bfadb62f41b5727672b388d81499437f23da6bb3
algunsprojetosdeDaniel <lojamarcketplaces@gmail.com>
2025-10-18 22:10:02 +0000

    escolher

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,67 @@
 #include <stdio.h>
-int main () {
-    int torre, bispo, rainha, cavalo1, cavalo2;
 
-    printf("A torre vai andar 5 casas para a direita\n");
 
-    torre = 1;
 
-    while (torre <= 5) {
-        printf("Direita\n");
-        torre++;
+void torre1 (int casastorre) {
+    if (casastorre > 0 && casastorre <= 5) {
+        printf("direita\n");
+        torre1(casastorre - 1);
     }
+}
 
-    printf("O bispo vai andar 5 casas na diagonal\n");
+void bispo1 (int casasbispo) {
+    if (casasbispo > 0 && casasbispo <= 5) {
+        printf("cima\n");
+        printf("direita\n");
+        bispo1(casasbispo -1);
+    }
+}
 
-    bispo = 1;
-
-    do {
-        printf("Cima\n");
-        printf("Direita\n");
-        bispo++;
-    } while (bispo <= 5);
-
-    printf("A rainha vai andar 8 casas para a esquerda\n");
-
-    rainha = 1;
-
-    for (rainha; rainha <= 8; rainha++) {
+void rainha1 (int casasrainha) {
+    if (casasrainha > 0 && casasrainha <= 8) {
         printf("esquerda\n");
+        rainha1(casasrainha - 1);
+    }
+}
+
+void cavalo1 (int casascavalo) {
+    casascavalo--;
+   for(casascavalo; casascavalo > 0; casascavalo--) {
+    printf("cima\n");
+   }
+   printf("direita\n");
+}
+
+int main () {
+    int torre, bispo, rainha, cavalo;
+
+    printf("Digite quantas casas é para a torre andar\n");
+    scanf("%d", &torre);
+
+    if(torre <= 5) {
+        torre1(torre);
     }
 
-    printf("O cavalo vai andar 2 casas para baixo e 1 casa para a esquerda\n");
+    printf("Digite quantas casas é para o bispo andar\n");
+    scanf("%d", &bispo);
 
-    cavalo1 = 1;
-    cavalo2 = 1;
-
-    for (cavalo2; cavalo2 <= 1; cavalo2++) {
-        do {
-            printf("Baixo\n");
-            cavalo1++;
-            
-        } while (cavalo1 <= 2);
-        printf("Esquerda\n");
-        cavalo2++;
+    if(bispo <= 5) {
+        bispo1(bispo);
     }
+    printf("Digite quantas casas é para a rainha andar\n");
+    scanf("%d", &rainha);
+
+    if (rainha <= 8) {
+        rainha1(rainha);
+    }
+
+    printf("Digite quantas casas é para o cavalo andar\n");
+    scanf("%d", &cavalo);
+
+    if(cavalo <= 3) {
+        cavalo1(cavalo);
+    }
+
 
     return 0;
 
